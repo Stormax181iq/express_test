311642251321c80cde51654fd3046dd676dd8197
Stormax181iq <kelianchalier@gmail.com>
2024-10-29 10:06:30 +0100

    add logout

--- server.js
+++ server.js
@@ -15,6 +15,10 @@ app.get("/register", (req, res) => res.render("register"));
 app.get("/login", (req, res) => res.render("login"));
 app.get("/admin", adminAuth, (req, res) => res.render("admin"));
 app.get("/basic", userAuth, (req, res) => res.render("user"));
+app.get("/logout", (req, res) => {
+  res.cookie("jwt", "", { maxAge: 1 });
+  res.redirect("/");
+});
 
 const server = app.listen(PORT, () => {
   console.log(`Server connected to port ${PORT}`);
@@ -24,5 +28,3 @@ process.on("unhandledRejection", (err) => {
   console.log(`An error occurred: ${err.message}`);
   server.close(() => process.exit(1));
 });
-// TODO stop at https://www.loginradius.com/blog/engineering/guest-post/nodejs-authentication-guide/#delete-users
-// TODO fix deleting user not working
